8fa668f85f843cd2f714a5d67da8e279cdcab771
Shaul Fridman <BlueSolei@users.noreply.github.com>
2017-12-03 12:31:16 +0200

    New windows machine scmachine

---
+++ NewWindowsMachin.bat
@@ -0,0 +1,29 @@
+@if not defined BatchVerbose (echo off)
+
+rem run this script as administrator
+if "%1"=="" (
+	Tools\bin\Elevate.exe InstallSoftware.bat elevated
+	exit /b %errorlevel%
+)
+
+echo Search for 'Chocolatey' package manager ...
+where choco > null
+if %errorlevel% NEQ 0 (
+	echo 'Chocolatey' wasn't found. installing it...
+	powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+) else (
+	echo 'Chocolatey' package manager was found.
+)
+
+echo.
+echo Install choco packages:
+choco install git python3 pip  -y
+choco upgrade all -y
+
+rem c++ package manager. 
+rem url: https://conanio.readthedocs.io/en/latest/introduction.html
+pip install conan
+
+rem semi-offical remote repository for conan packages, e.g. boost.
+rem url: https://bincrafters.github.io/
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
