a92bbc338a3239bbca0dd7ec3aba309207b880dd
Shaul Fridman <shaul.fridman@gmail.com>
2022-01-17 12:33:56 +0200

    (+) add packages

--- NewWindowsMachine.bat
+++ NewWindowsMachine.bat
@@ -23,21 +23,17 @@ rem choco packages:
 rem sharpkeys - mapping keys. used to map CapsLock to Esc for vim
 echo.
 echo Install choco packages:
-choco install git python3 sharpkeys 
+choco install git python3 GoogleChrome jetbrainstoolbox vscode mobaxterm tortoisegit microsoft-windows-terminal
 choco upgrade all 
 
 rem Phase 2
 rem ** Conan
 rem c++ package manager. 
 rem url: https://conanio.readthedocs.io/en/latest/introduction.html
-pip install conan
-rem semi-offical remote repository for conan packages, e.g. boost.
-rem url: https://bincrafters.github.io/
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+rem pip install conan
 
 rem ** Gui for bash
 rem how to add gui to bash at windows 10
 rem  	https://www.pcworld.com/article/3055403/windows/windows-10s-bash-shell-can-run-graphical-linux-applications-with-this-trick.html
 rem curl -o %temp%\Xming-6-9-0-31-setup.exe https://iweb.dl.sourceforge.net/project/xming/Xming/6.9.0.31/Xming-6-9-0-31-setup.exe
 rem %temp%\Xming-6-9-0-31-setup.exe
-
